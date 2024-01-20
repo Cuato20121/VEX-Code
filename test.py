40d103ef6d323430c2d5e0e22939aec445786d1e
Carson Edwards <cuato20121@gmail.com>
2024-01-19 23:51:21 -0500

    added files that have already been made

---
+++ test.py
@@ -0,0 +1,7 @@
+list = [5,6,8,3,7,9]
+otherlist = ["five","six","eight","three","seven","nine"]
+
+sortedlist = sorted(list)
+sortedotherlist = sorted(otherlist)
+print(sortedlist)
+print(otherlist)
